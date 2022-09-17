e3e7d485cbf22c462bf3a91e04ee0a4b07e147a4
agostinopique <agostino.pique@gmail.com>
2022-09-17 14:56:17 +0200

    Completate funzioni di elevazione, stampa e trasformazione degli array;

--- Program.cs
+++ Program.cs
@@ -1,4 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Metallica!");
 
-Console.WriteLine("Prova");
+#region PrintArray Function
+
+void PrintArray(int[] array)
+{
+    Console.Write("{");
+
+    int i;
+
+    for (i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i]);
+        Console.Write(", ");
+    }
+    Console.Write(array[i]);
+    Console.Write("}");
+}
+
+#endregion PrintArray Function
+int[] arrayNumeri = { 1, 2, 3 };
+
+//PrintArray(arrayNumeri);
+
+#region Quadrato function
+
+int Quadrato(int numero)
+{
+    return numero * numero;
+}
+
+#endregion Quadrato function
+
+#region Quadrato array function
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+
+    int[]arrayCopia = (int[])array.Clone();
+
+    for (int i = 0; i < arrayCopia.Length; i++)
+    {
+        arrayCopia[i] = Quadrato(arrayCopia[i]);
+    }
+
+    return arrayCopia;
+}
+
+PrintArray(ElevaArrayAlQuadrato(arrayNumeri));
+
+#endregion Quadrato array function
+
